4010c38065ab53cfe2914181e72dd98b4e2518ab
Nirav Shah <niravhgsc@gmail.com>
2011-04-26 07:56:28 -0500

    Formatting changes.

--- Anagrams.java
+++ Anagrams.java
@@ -8,39 +8,35 @@ import java.util.Arrays;
 
 public class Anagrams
 {
-  
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args)
-	{
-	  char content[];
-	  
+  public static void main(String[] args)
+  {
+    char content[] = null;
+    
     for(int i = 0; i < args.length; i++)
     {
-		  System.out.println(args[i]);
-			content = args[i].toCharArray();
-			Arrays.sort(content);
-			args[i] = new String(content);
-    }
-      
-    boolean notAnagram = false;
-      
-    for(int i = 0; !notAnagram && i < args.length - 1; i++)
-    {
-		 if(args[i].compareToIgnoreCase(args[i + 1]) != 0)
-		 {
-		  notAnagram = true;
-		 }
+      System.out.println(args[i]);
+      content = args[i].toCharArray();
+      Arrays.sort(content);
+      args[i] = new String(content);
     }
 
-    if(notAnagram == false)
-    {
-		 System.out.println("The words are anagrams");
-    }
-    else
-    {
-		 System.out.println("The words are not anagrams");
-    }
-	}
+    boolean isAnagram = true;
+
+   for(int i = 0; i < args.length -1 && isAnagram == true; i++)
+   {
+     if(args[i].compareToIgnoreCase(args[i + 1]) != 0)
+     {
+       isAnagram = false;
+     }
+   }
+
+   if(isAnagram == true)
+   {
+     System.out.println("The words are anagrams");
+   }
+   else
+   {
+     System.out.println("The words are not anagrams");
+   }
+  }
 }
